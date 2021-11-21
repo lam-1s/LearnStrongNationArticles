4e0d79410052342c21af2a1f24598e0ad308cbbe
Name Prohibited <public@scanf.ml>
2021-11-21 10:39:51 +0800

    add: script to delete old tags

---
+++ delete_tags.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+git fetch --tags
+TAGS=( $(git tag -l "*" | sort -r) )
+
+KEEP_LATEST=1
+
+LATEST_TAGS=()
+
+
+for TAG in ${TAGS[@]}; do
+    if [[ ${#LATEST_TAGS[@]} -lt ${KEEP_LATEST} ]]; then
+        LATEST_TAGS+=( "$TAG" )
+    fi
+done
+
+for TAG in ${LATEST_TAGS[@]}; do
+    TAGS=( "${TAGS[@]/$TAG}" )
+done
+
+for TAG in ${TAGS[@]}; do
+    echo "Deleting ${TAG}"
+    hub release delete "${TAG}"
+    git tag -d "${TAG}"
+done
+
+git push --tags --prune
